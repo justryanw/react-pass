2a35046f4e8c708653f865a7eb1dbc7dfbc6ca64
Ryan Walker <ryanjwalker2001@gmail.com>
2024-03-16 21:35:11 +0000

    basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import './style.css';
 import addIcon from './assets/add.svg';
 import trashIcon from './assets/trash.svg';
+import googleIcon from './assets/google.ico';
+
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,12 +13,21 @@ function App() {
     <>
       <div className="header-row">
         <div className='btn'>
-          <img src={addIcon} alt="Add" />
+          <img src={addIcon} alt="Add" className='icon' />
         </div>
         <div className='btn'>
-          <img src={trashIcon} alt="Delete" />
+          <img src={trashIcon} alt="Delete" className='icon' />
         </div>
       </div>
+      <ul className='list'>
+        <li className='login'>
+          <img className='favicon' src={googleIcon}></img>
+          <div>
+            <div className="title">Google</div>
+            <div className="username">someone@gmail.com</div>
+          </div>
+        </li>
+      </ul>
     </>
   );
 }
