7d348af75df1cbe49c00bb949aed8f016e9b10c2
Ryan Walker <ryanjwalker2001@gmail.com>
2024-03-17 20:16:14 +0000

    new login

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { LoginDetails } from './LoginDetails';
 
 export type DeleteLogin = (id: string) => void;
 export type SelectLogin = (id: string) => void;
+export type NewLogin = () => void;
 
 export interface Login {
   id: string,
@@ -44,19 +45,37 @@ export default function App() {
 
   const [selectedLogin, setSelectedLogin] = useState<Login | undefined>(logins[0]);
 
-  let deleteLogin: DeleteLogin = (id) => {
+  const deleteLogin: DeleteLogin = (id) => {
+    const lastIndex = logins.findIndex((login) => login.id === id);
     setLogins((currentLogins) => currentLogins.filter(login => login.id !== id));
-    setSelectedLogin(() => undefined);
+    const index = lastIndex + 1 !== logins.length ? lastIndex + 1 : lastIndex - 1;
+
+    setSelectedLogin(logins[index]);
   }
 
-  let selectLogin: SelectLogin = (id) => {
+  const newLogin: NewLogin = () => {
+    const newLogin = {
+      id: crypto.randomUUID(),
+      fields: new Map<string, string>([
+        ["Title", ""],
+        ["Username", ""],
+        ["Password", ""],
+        ["Url", ""],
+      ])
+    };
+
+    setLogins((currentLogins) => [...currentLogins, newLogin]);
+    setSelectedLogin(newLogin);
+  }
+
+  const selectLogin: SelectLogin = (id) => {
     setSelectedLogin(() => logins.find(login => login.id === id));
   }
 
   return (
     <>
       <div className="header-row">
-        <div className='btn'>
+        <div className='btn' onClick={() => newLogin()}>
           <img src={addIcon} alt="Add" className='icon' />
         </div>
         <div className='btn' onClick={() => selectedLogin && deleteLogin(selectedLogin.id)} >
